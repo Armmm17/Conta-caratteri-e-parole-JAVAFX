63f4319ec0c9e6ceaa1f094c3c3b354b15809916
Armando Boaca <armandoboaca@Armandos-iPro.station>
2024-03-09 14:07:11 +0100

    fix resizable

--- src/main/java/com/armmm17/contacaratterieparolejavafx/Main.java
+++ src/main/java/com/armmm17/contacaratterieparolejavafx/Main.java
@@ -29,7 +29,8 @@ public class Main extends Application {
 
         stage.setTitle(V.TITOLO);
         stage.setScene(scene);
-        stage.setResizable(true);
+        stage.setResizable(false);
+        
         stage.show();
     }
 
